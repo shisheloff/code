b099a4c6ff2ce6ba0fc78da1ba930030479c9dbc
SH_E <shishelovfc55@gmail.com>
2020-09-13 12:29:37 +0300

    master

---
+++ first.java
@@ -0,0 +1,7 @@
+
+class main{
+    public static void main (final String[] args) {
+        System.out.println("Hello");
+    }
+}
+
---
+++ golang.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 int
+	fmt.Scan(&num1, &num2)
+
+}
