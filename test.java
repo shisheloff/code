0f8ae908c4be8dd4c9ca7b6e9291175363c94fd2
shisheloff <45948320+shisheloff@users.noreply.github.com>
2020-10-13 21:23:59 +0300

    Add files via upload

---
+++ test.java
@@ -0,0 +1,28 @@
+package secondLab;
+
+public class test {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.addToHead(list, 5);
+        list.addToTail(list, 3);
+        list.addToTail(list, 7);
+        list.addToHead(list, 10);
+        list.addToHead(list, 2);
+        list.printList(list);
+        LinkedList clone = new LinkedList(list);
+        clone.printList(clone);
+        list.deleteByValue(2);
+        list.printList(list);
+        list.deleteByValue(10);
+        clone.printList(clone);
+        list.deleteByValue(5); 
+        list.printList(list);
+        clone.printList(clone);
+        list.counter(list);
+        clone.counter(clone);
+        clone.printList(clone);
+        list.deleteLinkedList();
+        list.printList(list);
+
+    }
+}
